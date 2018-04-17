0a9da55a42d6d681664d0826c91524ed745f839d
YegorLindberg <mr.tsarap@mail.ru>
2018-04-18 01:22:36 +0300

    mini-dictionary

---
+++ lw2/mini-dictionary/mini-dictionary/main.cpp
@@ -0,0 +1,125 @@
+//
+//  main.cpp
+//  mini-dictionary
+//
+//  Created by Moore on 17/04/2018.
+//  Copyright © 2018 Moore. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+const int FILE_IS_NOT_OPENED = 1;
+
+void ReadingMap(istream& inputFile, unordered_map<string, string>& dictionary);
+void PrintMap(ostream& outputFile, unordered_map<string, string>& dictionary);
+
+int main(int argc, const char * argv[]) {
+    
+    const string inputFileName = argv[1];
+    ifstream inputFile(inputFileName);
+    ofstream outputFile("output.txt");
+    if (!inputFile.is_open())
+    {
+        cout << "The File " << inputFileName << " can't be opened.\n";
+        return FILE_IS_NOT_OPENED;
+    }
+
+    unordered_map<string, string> dictionary;
+    ReadingMap(inputFile, dictionary);
+    
+    bool changeDictionary = false;
+    string wordToTranslate = "";
+    cout << " > ";
+    getline(cin, wordToTranslate);
+    while (wordToTranslate != "...")
+    {
+        dictionary.find(wordToTranslate);
+        unordered_map<string, string>::const_iterator got = dictionary.find(wordToTranslate);
+        if (got == dictionary.end())
+        {
+            cout << "Неизвестное слово/словосочетание \"" << wordToTranslate << "\". Введите перевод или пустую строку для отказа.\n";
+            string translateUnknown = "";
+            getline(cin, translateUnknown);
+            if (translateUnknown != "")
+            {
+                dictionary.emplace(pair<string, string>(wordToTranslate, translateUnknown));
+                cout << "Слово \"" << wordToTranslate << "\" сохранено в словаре как \"" << translateUnknown << "\".\n";
+                changeDictionary = true;
+            }
+            else
+                cout << "Слово \"" << wordToTranslate << "\" проигнорировано.\n";
+        }
+        else
+            cout << got->second << endl;
+        cout << " > ";
+        getline(cin, wordToTranslate);
+    }
+    if (changeDictionary == true)
+    {
+        cout << "В словаре были внесены изменения. Введите \"Y\" или \"y\" для сохранения перед выходом.\n > ";
+        string changes = "";
+        getline(cin, changes);
+        if ((changes == "Y") || (changes == "y"))
+        {
+            ofstream dictionaryAppend(inputFileName);
+            PrintMap(dictionaryAppend, dictionary);
+            cout << "Изменения сохранены. До свидания.\n";
+        }
+        else
+            cout << "Изменения не были сохранены. До свидания.\n";
+    }
+    
+    //PrintMap(outputFile, dictionary);
+    //cout << endl;
+    
+    return 0;
+}
+
+
+void ReadingMap(istream& inputFile, unordered_map<string, string>& dictionary)
+{
+    string engWord = "";
+    string ruWord = "";
+    bool translateWord = false;
+    char ch = '0';
+    while (!inputFile.eof())
+    {
+        if (translateWord == false)
+        {
+            while ((ch != '-') && (!inputFile.eof()))
+            {
+                inputFile >> ch;
+                if (ch != '-')
+                    engWord = engWord + ch;
+            }
+            translateWord = true;
+        }
+        else
+        {
+            inputFile >> ch;
+            getline(inputFile, ruWord);
+            ruWord.insert(ruWord.begin(), ch);
+            translateWord = false;
+        }
+        if (translateWord == false)
+        {
+            dictionary.emplace(pair<string, string>(engWord, ruWord));
+            engWord = "";
+            ruWord = "";
+        }
+    }
+}
+
+void PrintMap(ostream& outputFile, unordered_map<string, string>& dictionary)
+{
+    for (auto element = dictionary.begin(); element != dictionary.end(); ++element)
+    {
+        outputFile << element->first << "-" << element->second << endl;
+        cout << element->first << " - " << element->second << endl;
+    }
+}
